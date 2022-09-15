e4d9a48ebec651d507078eecc6ba2e6bdcac126a
Abdiel Corro <acorrop@outlook.com>
2022-09-15 12:51:35 -0500

    exercise 2 completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -5,13 +5,34 @@
         static void Main(string[] args)
         {
 
+            // Exercise 1
+
             var a = 17;
             var b = 4;
 
             var div = a / b;
-            var mod = a % b; 
+            var mod = a % b;
 
             Console.WriteLine($"{a}/{b} is {div} reminder {mod}");
+
+
+            //Exercise 2
+
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle wiht radius of {radius} is {AreaOfCircle(radius)}");
+            
+            AreaOfCircle(radius);
+
+
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
+
+                
         }
     }
 }
